8eb26f7e394099679c8d30c2d0e3f7bf38f6f390
LRekhviashvili <rekhviashvili089@gmail.com>
2022-05-22 14:01:50 +0400

    add form js

---
+++ script.js
@@ -0,0 +1,110 @@
+document.getElementById('mainForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    let errors = {};
+    let form = event.target;
+
+
+    let companyTitle = document.getElementById('companyTitle').value;
+    if(companyTitle.length>10 || companyTitle== ''){
+        errors.companyTitle = 'Name can not be empty and cannot be more than 10 symbols';
+    } 
+
+
+
+    let companyDescription = document.getElementById('companyDescription').value;
+    if(companyDescription.length>200 || companyDescription== ''){
+        errors.companyDescription = 'Description can not be empty and cannot be more than 200 symbols';
+    }
+
+    
+
+    let email = document.getElementById('email').value;
+    if(email== ''){
+        errors.email = 'Email is required';
+    }
+
+
+    let error = false;
+    let checkBoxDiv = document.getElementById('checkboxDiv');
+    checkBoxDiv.querySelectorAll('input[type="checkbox"]').forEach(element => {
+        if(element.checked){
+            error =true;
+        }
+    });
+    if (error == false) {
+        errors.quality = 'Please select'        
+    }
+
+
+    
+    let socialMedia = false;
+
+    form.querySelectorAll('[name="social"]').forEach(element => {
+        if(element.checked){
+            socialMedia =true;
+        }
+    });
+        if(socialMedia == false){
+            errors.social = 'Please select'
+        }
+        
+
+    form.querySelectorAll('.error-text').forEach(item => {
+        item.innerHTML = '';
+    });
+
+    let password = document.querySelector('[name="password"]').value;
+    let password1 = document.querySelector('[name="password1"]').value;
+
+    if (password!=='' && password!==password1){
+        errors.password1 = 'Your Password do not match';
+        errors.password = 'Your Password do not match';
+    }
+    if(password==''){
+        errors.password1 = 'Password can not be empty';
+        errors.password = 'Password can not be empty';
+}
+    for(let item in errors){
+        let errorSpan = document.getElementById('error_' + item);
+        if (errorSpan) {
+            errorSpan.textContent = errors[item];
+            
+        }
+    }
+
+    if (Object.keys(errors).length == 0) {
+        form.submit();
+    }
+});
+
+// ვალიდაცია პაროლებზე
+let passwordShow = document.getElementById('password');
+let toggleIcon = document.getElementById('toggleIcon');
+
+function showHidePassword() {
+    if(passwordShow.type == "password"){
+        passwordShow.setAttribute('type', 'text');
+        toggleIcon.classList.add('fa-eye-slash');
+    }else{
+        passwordShow.setAttribute('type', 'password');
+        toggleIcon.classList.remove('fa-eye-slash');
+    }   
+}
+
+toggleIcon.addEventListener('click', showHidePassword);
+
+// იმეილის ვალიდაცია
+
+function validation() {
+    let emailText = document.getElementById('email').value;
+    let spanText = document.getElementById('error_email'); 
+    let emailStructure = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (emailText.match(emailStructure)){
+        spanText.innerHTML = 'Your email is valid';
+        spanText.style.color = 'chartreuse';
+    }else{
+        spanText.innerHTML = 'Your email is not valid';
+        spanText.style.color = 'red';
+    }
+}
